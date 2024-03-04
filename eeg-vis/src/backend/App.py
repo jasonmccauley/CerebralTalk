84f264d095f19162f01febedc882804140edaa0c
omar <elkhafif.omar96@gmail.com>
2024-03-04 12:42:58 -0500

    Integrating confusion matrix with mongoDB

--- eeg-vis/src/backend/App.py
+++ eeg-vis/src/backend/App.py
@@ -1,4 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import scipy.io
+import io
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier # We can import more classifiers later, such as SVC and LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import base64
 from pymongo import MongoClient
 from flask_cors import CORS
 
@@ -11,7 +20,7 @@ client = MongoClient(mongo_uri)
 
 # Access the database and collection
 db = client.get_database("databaseScrumineers")
-collection = db.get_collection("collectionScrumineers")
+collection = db.get_collection("confusion_matrix")
 
 # Define routes
 @app.route("/")
@@ -23,16 +32,106 @@ def home():
 @app.route("/data", methods=["GET"])
 def get_data():
     # this retrieves data from MongoDB collection
-    data = list(collection.find({}))
+    data = collection.find_one({})
     return jsonify({"data": data})
 
-#specify app route for function, doesn't matter what it is but it will be used to call the function from React
-@app.route("/send_string", methods=["POST"])
-def send_string():
-    #creates dictionary
-    data = {'message': 'Hello from Flask!'}
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files: # Checking if the user hasn't uploaded a file, therefore would not appear in requests
+        return 'No file uploaded'
 
-    #converts data into a json and returns it (See HomePage.js for more)
-    return jsonify(data)
+    file = request.files['file'] # If file is uploaded, set assign file to the uploaded file
+
+    if file.filename == '': # Checking if the user submitted upload without selecting a file
+        return 'No file selected'
+
+    file_contents = file.read() # Read the uploaded file
+    file_obj = io.BytesIO(file_contents) # Create a file object from the memory of the file contents
+
+    data = scipy.io.loadmat(file_obj) # Load data from file object using scipy loadmat()
+    # Extract the structured array epo.y
+    epo_x = data['epo_train']['x'][0, 0]
+    epo_y = data['epo_train']['y'][0, 0]
+
+    #transpose
+    reshaped_data = epo_x.transpose(2, 0, 1)
+
+    # Create a dictionary to hold the data for the dataframe
+    data_dict = {}
+
+    # Extract the number of trials
+    num_trials = reshaped_data.shape[0]
+
+    for channel_idx in range(reshaped_data.shape[2]):
+        channel_data = reshaped_data[:, :, channel_idx].reshape(num_trials, -1)
+        flattened_channel_data = channel_data.flatten()  # Flatten the 2D array to 1D
+        data_dict[f'Channel_{channel_idx+1}'] = flattened_channel_data
+
+    # Create the dataframe
+    df = pd.DataFrame(data_dict)
+    
+    # Create a new dataframe with every 795th row
+    new_df = df.iloc[::795]
+
+    # Reset the index of the new dataframe
+    new_df.reset_index(drop=True, inplace=True)
+
+    # Create a list to hold the imagined speech for each trial
+    imagined_speech = []
+
+    # Mapping of row indices to speech labels
+    speech_labels = {
+        0: "Hello",
+        1: "Help me",
+        2: "Stop",
+        3: "Thank you",
+        4: "Yes"
+    }
+
+    # Iterate through each column of epo.y
+    for trial_idx in range(epo_y.shape[1]):
+        # Find the index of the row with a value of 1
+        speech_idx = int(epo_y[:, trial_idx].nonzero()[0])
+        
+        # Map the speech index to the corresponding label
+        if speech_idx in speech_labels:
+                imagined_speech.append(speech_labels[speech_idx])
+        else:
+                imagined_speech.append("None")
+
+    # Add the list as a new column in the dataframe
+    new_df['Imagined_Speech'] = imagined_speech
+
+    X = new_df.drop(columns=['Imagined_Speech']) # Change column name to the variable we are trying to predict, in our case, Imagined_Speech
+    y = new_df['Imagined_Speech']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42) # Split data into testing and training sets
+
+    clf = RandomForestClassifier() # Eventually add other classifiers, maybe through a dictionary?
+    clf.fit(X_train, y_train) # Train RandomForestClassifier()
+
+    y_pred = clf.predict(X_test) # Predict the target variable using the other attributes of the test set
+    accuracy = accuracy_score(y_test, y_pred) # Compare accuracy of predicted target variable vs. actual target variable
+
+    conf_matrix = confusion_matrix(y_test, y_pred) # Create a confusion matrix, then corresponding heatmap
+    plt.figure(figsize=(7,5))
+    sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='g')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Heatmap of Confusion Matrix')
+
+    buf = io.BytesIO() # Convert the heatmap of confusion matrix to base64-encoded image so that it could be json-ified
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    heatmap_image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    plt.close()
+    
+        # Save generated heatmap image to an appropriate location in the Mongo database
+    image_json = {'accuracy': accuracy, 'heatmap_image_base64': heatmap_image_base64}
+    images_db = db.get_collection("confusion_matrix")
+    images_db.insert_one(image_json)
+
+    return jsonify({'accuracy': accuracy, 'heatmap_image_base64': heatmap_image_base64})
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- eeg-vis/src/pages/HomePage.js
+++ eeg-vis/src/pages/HomePage.js
@@ -1,54 +1,90 @@
 import React from 'react';
 import { useState } from 'react';
+import axios from 'axios';
 
 function HomePage() {
-  const [imageSrc, setImageSrc] = useState("./logo192.png")
-  const [inputSrc, setInputSrc] = useState(imageSrc)
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [selectedClassifier, setSelectedClassifier] = useState('Random Forest'); 
+  const [accuracy, setAccuracy] = useState(null)
+  const [heatmapImage, setHeatmapImage] = useState(null)
+  const [isLoading, setIsLoading] = useState(false);
 
-  // async function used to call function from python, being async allows the function to contain "await"
-  const pullData = async () =>{
-    /*fetch() takes the app route that was defined for send_string() to call send_string()
-    response = the json returned by send_string()
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0]; // Takes first file selected by the user
+    if (file && file.name.endsWith('.mat')){ // Checking if uploaded file is MATLAB file
+      setUploadedFile(file); // Updates uploadedFile state with inputted file
+    }
+    else{
+      alert("Please upload a MATLAB file (ending in .mat)")
+    }
+  };
 
-    Since data is being fetched from another server, "await" is used to ensure the rest of the function doesn't execute until
-    the data has been fetched and stored in variable response
+  const handleClassifierChange = (event) => {
+    setSelectedClassifier(event.target.value); // Updates selectedClassifier state according to value selected in dropdown
+  };
 
-    "await" basically tells the program to pause any more code execution within the function until the line of code with "await" 
-    is consindered to be complete (look up JS promises for more on this)
+  const handleAnalysis = async () => {
+    if (!uploadedFile){
+      alert("Please upload a file");
+      return;
+    }
+
+    const formData = new FormData(); // Creates an instance called formData, and appends the uploadedFile to send to backend
+    formData.append('file', uploadedFile);
+    setIsLoading(true);
+    try{
+      const response = await axios.post('/upload', formData, { // Sends post request to backend with formData, which stores uploadedFile
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
     
-    In this case we're telling pullData() "after you try fetching data don't do anything else until you have that json from flask"
+      const {accuracy, heatmap_image_base64} = response.data; // Returns accuracy and heatmap in base64 from the response data, since these were returned in the backend
+      setAccuracy(accuracy); // Update state varaibles for accuracy and heatmapImage
+      setHeatmapImage(heatmap_image_base64);
+    } 
+    catch(error){
+      console.error('Error: ', error);
+    }
+    finally {
+      setIsLoading(false);
+    }
+  };
 
-    Without "await" the rest of the function executes regardless of if the fetch request has been completed which means the
-    variable response may not contain the fetched json by the time it tries to parse it and/or print it
-    */
-    const response = await fetch('/send_string', {
-      method: 'POST',
-    })
-    //parses the json
-    const jsonData = await response.json();
 
-    //prints the json to the console
-    console.log(jsonData)
-  }
-
-  const updateImage = () => {
-    setImageSrc(inputSrc)
-  }
-  //calls pullData()
-  pullData()
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to our website!</p>
-      
+
       <div>
-        <input 
-            placeholder="file path" 
-            onChange={(e) => setInputSrc(e.target.value)}
-          />
-        <button onClick={updateImage}>Display image</button>
+        <input type="file" accept=".mat" onChange={handleFileUpload}/>
       </div>
-      <img src={imageSrc} alt="text"/>
+
+      <div>
+        <label htmlFor="classifier">Select Classifier:</label>
+        <select id="classifier" value={selectedClassifier} onChange={handleClassifierChange}>
+          <option value="RandomForest">Random Forest</option>
+        </select>
+      </div>
+
+      <div>
+        <button onClick={handleAnalysis} disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Analyze'}
+        </button>
+      </div>
+
+      {accuracy !== null && (
+        <div>
+          <p>Accuracy: {accuracy}</p>
+        </div>
+      )}
+
+      {heatmapImage && (
+        <div>
+          <img src={`data:image/jpeg;base64,${heatmapImage}`} alt='Heatmap' />
+        </div>
+      )}
     </div>
   );
 }
