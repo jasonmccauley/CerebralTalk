076dbb7e3233946c94bb7e7b3af44b953f9c2f80
Benjamin Knobloch <bknobloc@stevens.edu>
2024-04-01 14:10:46 -0400

    Refactored classifier result displays into its own component to simplify AnalysisResults.js

--- eeg-vis/src/components/AnalysisResults.js
+++ eeg-vis/src/components/AnalysisResults.js
@@ -1,14 +1,13 @@
 import React from "react";
+import ClassifierResult from "./ClassifierResult";
 
 function AnalysisResults({ comparison, classifier, secondClassifier, accuracy, secondAccuracy, removedChannels, heatmapImage, secondHeatmapImage}){
     return (
         
         comparison ?
             
-            (<div>
-                <div>
-                    {/* Separate div for the comparison message to ensure it takes full width */}
-                    <div className="comparison-message">
+            (<div >
+                <div className="comparison-message">
                     {accuracy > secondAccuracy ? (
                         <p>Random Forest has a higher accuracy at prediction than Logistic Regression for this data set.</p>
                     ) : accuracy < secondAccuracy ? (
@@ -16,54 +15,49 @@ function AnalysisResults({ comparison, classifier, secondClassifier, accuracy, s
                     ) : (
                         <p>Random Forest and Logistic Regression have equal accuracy at prediction of this data set.</p>
                     )}
+                </div>
+            
+                <br/>
+
+                <div className='comparison-results'>
+                    
+                    <div className='classifier-result'>
+                        <ClassifierResult
+                            classifier={classifier}
+                            accuracy={accuracy}
+                            removedChannels={removedChannels}
+                            heatmapImage={heatmapImage}
+                            label="Classifier 1"
+                        />
                     </div>
-                    <div className="comparison-results">
-                    <div className="classifier-result">
-                        <h2>Classifier 1: {classifier}</h2>
-                        <p>Accuracy: {accuracy}</p>
-                        {removedChannels.length !== 0 && (
-                        <p>Removed channels: {removedChannels.join(', ')}</p>
-                        )}
-                        {heatmapImage && (
-                        <div>
-                            <img src={`data:image/jpeg;base64,${heatmapImage}`} alt='Heatmap 1' />
-                        </div>
-                        )}
+                    
+                    <div className='classifier-result'>
+                        <ClassifierResult
+                            classifier={secondClassifier}
+                            accuracy={secondAccuracy}
+                            removedChannels={removedChannels}
+                            heatmapImage={secondHeatmapImage}
+                            label="Classifier 2"
+                        />
                     </div>
-                    <div className="classifier-result">
-                        <h2>Classifier 2: {secondClassifier}</h2>
-                        <p>Accuracy: {secondAccuracy}</p>
-                        {removedChannels.length !== 0 && (
-                        <p>Removed channels: {removedChannels.join(', ')}</p>
-                        )}
-                        {secondHeatmapImage && (
-                        <div>
-                            <img src={`data:image/jpeg;base64,${secondHeatmapImage}`} alt='Heatmap 2' />
-                        </div>
-                        )}
-                    </div>
-                    </div>
+                
                 </div>
             </div>)
             
             :
             
             (
+            <div className='comparison-results'>
                 <div>
-                    {classifier ?(
-                        <h2>Classifier: {classifier}</h2>
-                    ):(
-                        <h2>Classifier: Random Forest</h2>
-                    )}
-                    
-                    <p>Accuracy: {accuracy}</p>
-                    {removedChannels.length !== 0 ? (
-                        <p>Removed channels: {removedChannels.join(', ')}</p>
-                    ):(
-                        <p>Removed channels: none</p>
-                    )}
-                    <img src={`data:image/jpeg;base64,${heatmapImage}`} alt='Heatmap' />
+                    <ClassifierResult
+                        classifier={classifier}
+                        accuracy={accuracy}
+                        removedChannels={removedChannels}
+                        heatmapImage={heatmapImage}
+                        label="Heatmap"
+                    />
                 </div>
+            </div>
             )
     )
 }
---
+++ eeg-vis/src/components/ClassifierResult.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ClassifierResult = ({ classifier, accuracy, removedChannels, heatmapImage, label }) => (
+    <>
+        <h2>{label}: {classifier}</h2>
+        <p>Accuracy: {accuracy}</p>
+        {removedChannels.length !== 0 && (
+            <p>Removed channels: {removedChannels.join(', ')}</p>
+        )}
+        {heatmapImage && (
+            <div>
+                <img src={`data:image/jpeg;base64,${heatmapImage}`} alt={`Heatmap for ${label}`} />
+            </div>
+        )}
+    </>
+);
+
+export default ClassifierResult;
