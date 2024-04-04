2ef9bd57e05f05ef85bd4607a4a3a58545b5c477
mamorales5523 <90879620+mamorales5523@users.noreply.github.com>
2024-04-04 17:13:11 -0400

    displays graphs corresponding to speech

--- eeg-vis/src/components/SearchSpeech.js
+++ eeg-vis/src/components/SearchSpeech.js
@@ -18,24 +18,32 @@ const useStyles = makeStyles((theme) => ({
 function SearchSpeech({ graphsBase64 }){
     const classes = useStyles();
     const [selectedSpeech, setSelectedSpeech] = useState("")
-    const keys = Object.keys(graphsBase64)
+    let keys = []
 
+    if(graphsBase64){
+        keys = Object.keys(graphsBase64)
+    }else{
+        keys = ["No imagined speech labels found"]
+    }
     return(
         <Grid item>
-          <FormControl>
-            <InputLabel id="speech-label" style={{ color: 'white' }}>Select Imagined Speech:</InputLabel>
-            <Select 
-              labelId="speech-label" 
-              value={selectedSpeech} 
-              //onChange={handleClassifierChange} 
-              className={classes.select}
-              IconComponent={(props) => <span {...props} style={{ color: 'rgb(197, 197, 246)' }}>&#9660;</span>}
-            >
-              {keys.map((key, index) => (
-                <MenuItem key={index} value={key}>{key}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+            <FormControl>
+                <InputLabel id="speech-label" style={{ color: 'white' }}>Select Imagined Speech:</InputLabel>
+                <Select 
+                labelId="speech-label" 
+                value={selectedSpeech} 
+                onChange={(event) => setSelectedSpeech(event.target.value)} 
+                className={classes.select}
+                IconComponent={(props) => <span {...props} style={{ color: 'rgb(197, 197, 246)' }}>&#9660;</span>}
+                >
+                {keys.map((key, index) => (
+                    <MenuItem key={index} value={key}>{key}</MenuItem>
+                ))}
+                </Select>
+            </FormControl>
+            {selectedSpeech &&(
+                <img src={`data:image/jpeg;base64,${graphsBase64[selectedSpeech]}`} alt={`Graph for ${selectedSpeech}`} />
+            )}
         </Grid>
     )
 }
